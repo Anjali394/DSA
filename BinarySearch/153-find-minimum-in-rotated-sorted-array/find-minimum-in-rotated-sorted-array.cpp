ef3dafd02a0b285f378becb930168c2085e88b92
Anjali Rajput <anjalirajput394@gmail.com>
2026-06-22 21:21:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (10.09%) - LeetSync

--- BinarySearch/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ BinarySearch/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -7,14 +7,16 @@ public:
             int mid = l+ (h-l)/2;
             int prev = (mid-1+n)%n;
             int next = (mid+1)%n;
+            
             if(nums[prev]>=nums[mid] && nums[next]>= nums[mid]){
                 return nums[mid];
-            }else if(nums[h]>= nums[mid]){
+            }
+            else if(nums[h]>= nums[mid]){
                 h = mid-1;
             }else if(nums[l]<=nums[mid]){ // sorted left
                 l = mid+1;
             }
         }
-        return 0;
+        return nums[0];
     }
 };
