fc3234a6f1dffdb71ca32588c6911fb898b2034f
Anjali Rajput <anjalirajput394@gmail.com>
2026-06-23 12:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (64.40%) - LeetSync

--- BinarySearch/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ BinarySearch/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int l =0;
         int h = numbers.size()-1;
-        vector<int>ans;
         while(l<=h){
             int sum = numbers[l] + numbers[h];
             if(sum == target){
-                ans.push_back(l+1);
-                ans.push_back(h+1);
-                return ans;
+                return {l+1, h+1};
             } else if(sum<target){
                 l++;
             }else{
                 h--;
             }
         }
-        return ans;
+        return {};
     }
 };
