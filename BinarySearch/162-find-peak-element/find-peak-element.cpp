325b97fd5480e2da49c14d0fafaed6eb4410f97f
Anjali Rajput <anjalirajput394@gmail.com>
2026-06-23 10:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (39.13%) - LeetSync

---
+++ BinarySearch/162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        // o(log n) - divide array in half
+        int n = nums.size();
+        int l = 0, h = n-1;
+
+        while(l<h){
+            int mid = l +(h-l)/2;
+            if(nums[mid]< nums[mid+1]){
+                l = mid+1;
+            } else {
+                h = mid;
+            }
+        }
+        return l;
+        
+    }
+};
