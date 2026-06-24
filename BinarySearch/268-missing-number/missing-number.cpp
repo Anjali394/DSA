41b7f49ef7b50a86429b81a6c1122a30c7ffee26
Anjali Rajput <anjalirajput394@gmail.com>
2026-06-24 15:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (69.91%) - LeetSync

--- BinarySearch/268-missing-number/missing-number.cpp
+++ BinarySearch/268-missing-number/missing-number.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-       unordered_map<int, int> mp;
-        for(int i =0; i<n; i++){
-            mp[nums[i]]++; 
-        }
-        for (int i =0; i<=n; i++){
-            if(mp.find(i)==mp.end()){
-                return i;
-            }
-        }
+    //    unordered_map<int, int> mp;
+    //     for(int i =0; i<n; i++){
+    //         mp[nums[i]]++; 
+    //     }
+    //     for (int i =0; i<=n; i++){
+    //         if(mp.find(i)==mp.end()){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
 
-        return -1;
+
+    // O(1) - approach - sum of first n numbers = n * (n+1)/2;
+
+    int expectedSum = n * (n+1)/2;
+    int actual = 0;
+    for(int i =0 ;i<n; i++){
+        actual = actual+nums[i];
+    }
+
+    return expectedSum-actual;
     }
 };
