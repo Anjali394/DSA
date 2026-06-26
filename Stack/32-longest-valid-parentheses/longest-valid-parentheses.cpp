374e87fbd66e04d8184b8ee649a09810935b1bce
Anjali Rajput <anjalirajput394@gmail.com>
2026-06-26 21:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (51.77%) - LeetSync

--- Stack/32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ Stack/32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -15,7 +15,7 @@ public:
                 if (st.empty()) {
                     st.push(i);
                 } else {
-                    ans = max(ans, i - st.top());
+                    ans = max(ans, i-st.top());
                     // The stack stores indices, not characters.
                     // the current valid substring starts at st.top() + 1 and ends at i.
                     // i - st.top() -->length of valid substring
