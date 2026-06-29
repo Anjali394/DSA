a673e758e9f5386dc7b347c4baed48ae8fc4e14a
Anjali Rajput <44219212+Anjali394@users.noreply.github.com>
2026-06-29 23:55:45 +0530

    Time: 29 ms (29.94%) | Memory: 85.8 MB (34.18%) - LeetSync

---
+++ Stack/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+
+        int n = heights.size();
+
+        vector<int> nsl(n), nsr(n);
+        stack<int> st;
+
+        // Nearest Smaller to Left
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            if (st.empty())
+                nsl[i] = -1;
+            else
+                nsl[i] = st.top();
+
+            st.push(i);
+        }
+
+        while (!st.empty())
+            st.pop();
+
+        // Nearest Smaller to Right
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            if (st.empty())
+                nsr[i] = n;
+            else
+                nsr[i] = st.top();
+
+            st.push(i);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int width = nsr[i] - nsl[i] - 1;
+            ans = max(ans, heights[i] * width);
+        }
+
+        return ans;
+    }
+};
