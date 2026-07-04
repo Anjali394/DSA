407f3544c068657972d8cb901800abbfb8ddebbc
Anjali Rajput <44219212+Anjali394@users.noreply.github.com>
2026-07-04 23:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (80.41%) - LeetSync

---
+++ Heap/1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        priority_queue<int>maxh;
+
+        for(int i=0; i<n; i++){
+            maxh.push(stones[i]);
+            
+        }
+        while(maxh.size()>=2){
+                int x = maxh.top();
+                maxh.pop();
+                int y = maxh.top();
+                maxh.pop();
+
+                if(x != y)
+                {
+                    maxh.push(abs(y-x));
+                } else {
+                    maxh.push(0);
+                }
+            }
+       return maxh.top();
+        
+    }
+};
